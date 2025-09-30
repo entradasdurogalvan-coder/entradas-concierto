0c3c31d99d924518bcb83eaf7053a0e818db990f
entradasdurogalvan-coder <entradasdurogalvan@gmail.com>
2025-09-30 14:26:57 +0200

    Update ticket-count.js

--- netlify/functions/ticket-count.js
+++ netlify/functions/ticket-count.js
@@ -1,17 +1,30 @@
 const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwVsEuzPq-efkDmmEWo9rUtrsGylUNzkETLlaczHw2O0LrflSQrGj8DF7rQrj5dUw1e/exec';
 
 exports.handler = async function(event, context) {
+  // Headers para CORS
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
   try {
     const response = await fetch(GOOGLE_SCRIPT_URL + '?action=count');
     const data = await response.json();
     
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify(data)
     };
   } catch (error) {
     return {
-      statusCode: 500,
+      statusCode: 200,
+      headers,
       body: JSON.stringify({ 
         count: 0, 
         available: 100,
