94169fc883a209c6679690dbfdd27b4a1643e18d
entradasdurogalvan-coder <entradasdurogalvan@gmail.com>
2025-09-30 17:00:31 +0200

    Update create-reservation.js

--- netlify/functions/create-reservation.js
+++ netlify/functions/create-reservation.js
@@ -1,26 +1,35 @@
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxTPmxWmVoZbTqa4Ds7mJjF1v56iscKN_o_Xs2kWn_jHOq7Ox6a_d-QV_5Wo-R9nxZy0w/exec';
+const fetch = require('node-fetch');
+const querystring = require('querystring');
 
 exports.handler = async function(event, context) {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
-  try {
-    const response = await fetch(GOOGLE_SCRIPT_URL + '?action=create', {
-      method: 'POST',
-      body: event.body
-    });
+  const data = JSON.parse(event.body);
 
-    const data = await response.json();
-    
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data)
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Error creando reserva' })
-    };
-  }
+  // Convertir los datos a form data
+  const formData = querystring.stringify({
+    operation: 'create-reservation',
+    name: data.name,
+    email: data.email,
+    phone: data.phone,
+    ticketType: data.ticketType,
+    price: data.price
+  });
+
+  const response = await fetch('https://script.google.com/macros/s/AKfycbxTPmxWmVoZbTqa4Ds7mJjF1v56iscKN_o_Xs2kWn_jHOq7Ox6a_d-QV_5Wo-R9nxZy0w/exec', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: formData
+  });
+
+  const responseData = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseData)
+  };
 };
